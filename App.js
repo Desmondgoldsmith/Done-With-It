7fc2797c8281210b87cc848ccecf1aeda6ba20dd
Desmond Goldsmith JR <desmondgolsmith07@gmail.com>
2021-09-24 01:40:45 -0700

    Update👀😍

--- App.js
+++ App.js
@@ -1,13 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View , SafeAreaView, Button, Alert} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{
+        width:80,
+        height:80,
+        backgroundColor:"green",    
+
+      }}>
+
+<Text> Crazy Ass </Text>
+      </View>
+      <Text>Hello Dessy😍</Text>
+      <Image source={{
+        width:100,
+        height:100,
+        uri:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0EE5I6jOyzowFq9XC-zbVXhkRsz4QrMe2Ll-7q2gx4saqhwxytJvBUkf-IjBAmGms4_Q&usqp=CAU"
+      }}/>
+      <StatusBar style="auto" />  
+<Button title="Hit Me!" 
+onPress = {() => Alert.alert("Dessy Is King","New To React",[
+  {text:"Yes"},
+  {text:"No!"}
+])} />
+    </SafeAreaView>
   );
 }
 
